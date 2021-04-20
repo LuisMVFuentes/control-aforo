d7a991e6de23880e3c93c550312f7ca7478f2965
LuisMVFuentes <lmvf_123@hotmail.com>
2021-04-20 09:55:11 -0500

    505 Server Error

---
+++ resources/views/errors/505.blade.php
@@ -0,0 +1,18 @@
+@extends('template')
+
+
+@section('title', '505 ERROR')
+
+@section('content')
+
+
+<div class="container">
+    <div class=" row align-items-center justify-content-center vh-100">
+        <div class="col sm-6 text-center">
+            <h1><img src="{{ asset('img/control-af.png')}}" width="15%" height="15%"> Error 505 | Server Error</h1>
+            <br>
+            <a href="{{ route('home')}}" class="btn btn-primary">Regresar a Inicio</a>
+        </div>
+    </div>
+</div>
+@endsection
